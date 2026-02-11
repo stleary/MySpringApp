010459021218c20273120f788005140a1db7eaef
Sean Leary <stleary@Seans-MacBook-Pro.local>
2026-02-11 10:11:45 -0600

    add /greeting endpoint with optional name requestParam

--- src/main/java/com/example/Example.java
+++ src/main/java/com/example/Example.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -15,8 +16,12 @@ public class Example {
         SpringApplication.run(Example.class, args);
     }
 
+    @GetMapping("/greeting")
+    public String index(@RequestParam(defaultValue = "unknown") String name) {
+        return "Greetings to " + name + " from Spring Boot! /greeting";
+    }
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from Spring Boot! /";
     }
 }
